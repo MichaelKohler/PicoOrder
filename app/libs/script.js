66e9eff60fc92e331ed04d0fb9b3ce91982984e2
Michael Kohler <mkohler@picobudget.com>
2014-01-03 18:24:39 +0100

    new theme (menu not yet working)

---
+++ app/libs/script.js
@@ -0,0 +1,130 @@
+
+$(document).ready(function(e) {
+	$('.with-hover-text, .regular-link').click(function(e){
+		e.stopPropagation();
+	});
+	
+	/***************
+	* = Hover text *
+	* Hover text for the last slide
+	***************/
+	$('.with-hover-text').hover(
+		function(e) {
+			$(this).css('overflow', 'visible');
+			$(this).find('.hover-text')
+				.show()
+				.css('opacity', 0)
+				.delay(200)
+				.animate(
+					{
+						paddingTop: '25px',
+						opacity: 1
+					},
+					'fast',
+					'linear'
+				);
+		},
+		function(e) {
+			var obj = $(this);
+			$(this).find('.hover-text')
+				.animate(
+					{
+						paddingTop: '0',
+						opacity: 0
+					},
+					'fast',
+					'linear',
+					function() {
+						$(this).hide();
+						$( obj ).css('overflow', 'hidden');
+					}
+				);
+		}
+	);
+	
+	var img_loaded = 0;
+	var j_images = [];
+	
+});
+
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+function menu_focus( element, i ) {
+	if ( $(element).hasClass('active') ) {
+		if ( i == 6 ) {
+			if ( $('.navbar').hasClass('inv') == false )
+				return;
+		} else {
+			return;
+		}
+	}
+		
+	if ( i == 1 || i == 6 )
+		$('.navbar').removeClass('inv');
+	else
+		$('.navbar').addClass('inv');
+	
+	$('.nav > li').removeClass('active');
+	$(element).addClass('active');
+	
+	var icon = $(element).find('.icon');
+	
+	var left_pos = icon.offset().left - $('.nav').offset().left;
+	var el_width = icon.width() + $(element).find('.text').width() + 10;
+	
+	$('.active-menu').stop(false, false).animate(
+		{
+			left: left_pos,
+			width: el_width
+		},
+		1500,
+		'easeInOutQuart'
+	);
+}
+
+/***************
+* = Menu hover *
+***************/
+jQuery(document).ready(function ($) {
+	//Cache some variables
+	var menu_item = $('.nav').find('li');
+	
+	menu_item.hover(
+		function(e) {
+			var icon = $(this).find('.icon');
+			
+			var left_pos = icon.offset().left - $('.nav').offset().left;
+			var el_width = icon.width() + $(this).find('.text').width() + 10;
+			
+			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
+				.css('left', left_pos)
+				.css('width', el_width)
+				.attr('id', 'special-active-menu-' + $(this).data('slide') );
+			
+			$('.active-menu').after( hover_bar );
+		},
+		function(e) {
+			$('.special-active-menu').remove();
+		}
+	);
+});
+
+$(document).ready(function(e) {
+    var lis = $('.nav > li');
+    menu_focus( lis[0], 1 );
+
+    $(".fancybox").fancybox({
+        padding: 10,
+        helpers: {
+            overlay: {
+                locked: false
+            }
+        }
+    });
+});
